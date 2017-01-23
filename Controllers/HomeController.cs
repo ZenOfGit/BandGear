1fb9b69f8c17301e676a29557d484af5de278a41
Brad Miller <halfpipe@charter.net>
2017-01-22 23:17:38 -0600

    Redo with .Net core

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BandGear.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new BandGear.Models.Guitar();
+            model.Name = "Stratocaster";
+            return View(model);
+        }
+
+
+        public IActionResult Create(string GuitarName)
+        {
+            var model = new BandGear.Models.Guitar();
+            model.Name = GuitarName;
+            return View("Index", model);
+        }
+    }
+}
